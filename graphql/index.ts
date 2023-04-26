e8c867f1e3be8349aec2fb6524ce5fe15733f62e
codemaverick-uta <vaibhav.soni2@mavs.uta.edu>
2023-04-25 22:45:00 -0500

    removed CORS

--- graphql/index.ts
+++ graphql/index.ts
@@ -1,30 +1,30 @@
-import { ApolloServer, gql} from "apollo-server-azure-functions";
+const { ApolloServer, gql } = require('apollo-server-azure-functions');
+const {
+  ApolloServerPluginLandingPageLocalDefault
+} = require('apollo-server-core');
 
-// Construct a schema, using GraphQL schema language
+// Schema definition.
 const typeDefs = gql`
   type Query {
     hello: String
   }
 `;
 
-// Provide resolver functions for your schema fields
+// Resolver map.
 const resolvers = {
   Query: {
-    hello: () => "Hello from our GraphQL backend!",
+    hello: () => 'Hello world!',
   },
 };
 
-// @ts-ignore
+// Create our server.
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  debug: true
+  csrfPrevention: true,
+  cache: 'bounded',
+  plugins: [
+    ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+  ],
 });
-
-export default server.createHandler({
-  cors: {
-    origin: ['*', "https://studio.apollographql.com"],
-    methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["access-control-allow-credentials", "access-control-allow-origin", "content-type"]
-  },
-});
+exports.graphqlHandler = server.createHandler();
